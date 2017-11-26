f0775a460081920d24eb24ed0ab7abc81098748d
Mark Hsu <mark.g.hsu@gmail.com>
2017-11-25 19:59:29 -0800

    feat: add status bar

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, StatusBar } from 'react-native'
 import DeckList from './components/DeckList'
+import {white,orange} from './utils/colors'
+import {Constants} from 'expo'
 
 export default class App extends React.Component {
   render() {
     return (
-      <DeckList />
+      <View style={styles.container}>
+        <UStatusBar backgroundColor={orange} barStyle='light-content'/>
+        <DeckList decks={decks}/>
+      </View>
     );
+  }
+}
+
+function UStatusBar({ backgroundColor, ...props}) {
+    return (
+    <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props}/>
+    </View>
+  )
+}
+
+//TEMPORARY HARDCODED
+const decks = {
+  React: {
+    title: 'React',
+    questions: [
+      {
+        question: 'What is React?',
+        answer: 'A library for managing user interfaces'
+      },
+      {
+        question: 'Where do you make Ajax requests in React?',
+        answer: 'The componentDidMount lifecycle event'
+      }
+    ]
+  },
+  JavaScript: {
+    title: 'JavaScript',
+    questions: [
+      {
+        question: 'What is a closure?',
+        answer: 'The combination of a function and the lexical environment within which that function was declared.'
+      }
+    ]
   }
 }
 
@@ -14,7 +53,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
