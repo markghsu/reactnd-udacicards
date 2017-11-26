a71a6182a45036f567108b6c3ba426be5fa13a79
Mark Hsu <mark.g.hsu@gmail.com>
2017-11-25 20:00:22 -0800

    feat: basic decklist functionality

--- components/DeckList.js
+++ components/DeckList.js
@@ -7,7 +7,33 @@ export default class DeckList extends Component {
 		return (
 			<View>
 				<Text>DeckList</Text>
+				<View style={styles.container}>
+					{Object.keys(this.props.decks).map((keyName,keyIndex)=>{
+						const deck = this.props.decks[keyName]
+						
+						return (<View key={keyName}>
+							<Text style={styles.title}>{deck.title}</Text>
+							<Text style={styles.subheader}>{deck.questions.length} card{deck.questions.length!==1?"s":""}</Text>
+						</View>)			
+					})}
+				</View>
 			</View>
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+ container:{
+
+  },
+  title: {
+    color: '#000',
+    fontSize: 24,
+    textAlign: 'center',
+  },
+  subheader: {
+  	color:'#333',
+  	fontSize: 20,
+  	textAlign: 'center'
+  }
+});
