311577f6f5869eec9b4575ce910bba778839fd34
Mark Hsu <mark.g.hsu@gmail.com>
2017-11-26 18:17:20 -0800

    feat: scaffold add card form

---
+++ components/AddCard.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react'
+import {View, Text, TextInput, StyleSheet, TouchableOpacity} from 'react-native'
+import {lightPurp, white} from '../utils/colors'
+
+export default class AddCard extends Component {
+	state = {
+		question:"",
+		answer:""
+	}
+	submit=()=>{
+		//Update store
+		//Update in DB
+		//Navigate to back to deck
+	}
+	changeQuestion= (question) => {
+		this.setState(()=>({
+			question
+		}))
+	}
+	changeAnswer= (answer) => {
+		this.setState(()=>({
+			answer
+		}))
+	}
+	render() {
+		const {answer,question} = this.state
+		return (
+			<View style={styles.container}>
+				<TextInput value={question} placeholder="Enter your question" style={styles.field} onChangeText={this.changeQuestion} />
+				<TextInput value={answer} placeholder="Enter your answer" style={styles.field} onChangeText={this.changeAnswer} />
+				<TouchableOpacity style={styles.button} onPress={this.submit}>
+					<Text style={{textAlign:'center',color:white}}>Submit</Text>
+				</TouchableOpacity>
+			</View>
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+	container:{
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: lightPurp
+	},
+	field: {
+		alignSelf: 'stretch', 
+		height: 44,
+		padding: 8,
+		borderWidth: 1,
+		borderColor: '#777',
+		margin: 50,
+		backgroundColor: white
+	},
+	button: {
+		width: 200,
+		height: 50,
+		padding: 10,
+		borderWidth: 2,
+		alignContent: 'center',
+		justifyContent: 'center',
+		backgroundColor: 'black',
+	}
+});
