dbd419cf7b5568ef1893c0e767b3c66e5bca4346
Mark Hsu <mark.g.hsu@gmail.com>
2017-11-28 23:57:21 -0800

    feat: update API endpoints

--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,30 @@
 import { AsyncStorage } from 'react-native'
 const DECK_KEY = 'Udacicards:deck'
 
-export function fetchAllDecks() {
-	return AsyncStorage.getItem(DECK_KEY)
+export function getDecks() {
+	return AsyncStorage.getItem(DECK_KEY).then((result)=>{
+		const data = JSON.parse(result)
+		return data
+	})
+}
+
+export function getDeck(id) {
+	return AsyncStorage.getItem(DECK_KEY).then((result)=>{
+		const data = JSON.parse(result)
+		return data[id]
+	})
+}
+
+export function saveDeckTitle(title){
+	//DOES NOT CHECK IF DECK ALREADY EXISTS
+	//TODO: CHECK IF DECK EXISTS ALREADY
+	return AsyncStorage.mergeItem(DECK_KEY, JSON.stringify({
+		[title]:{title}
+	})
+}
+
+export function addCardToDeck(title,card){
+	return AsyncStorage.mergeItem(DECK_KEY, JSON.stringify({
+		[title]:{questions:[card]}
+	})
 }
