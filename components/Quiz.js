eceede74cb80299e9628532ab594240c3b639c06
Mark Hsu <mark.g.hsu@gmail.com>
2017-12-19 23:45:14 -0800

    Feat: Add correct/incorrect state and question increment

--- components/Quiz.js
+++ components/Quiz.js
@@ -6,30 +6,47 @@ import ShowCard from './ShowCard'
 
 class Quiz extends Component {
 	state = {
-		questionNum:0
+		questionNum:0,
+		numCorrect:0,
+		numIncorrect:0,
 	}
 	nextQuestion=()=>{
 		
 		if(this.state.questionNum === this.props.deck.questions.length - 1){
 			//Last element
+			alert(this.state.numCorrect+' correct '+this.state.numIncorrect+' incorrect.')
 		}
 		else {
 			this.setState((st)=>{
-				const questionNum = st.questionNum + 1;
 				return {
-					questionNum
+					questionNum:st.questionNum + 1
 				}
 			})
 		}
 	}
 	correctQuestion= () => {
+		this.setState((st)=>{
+			return {
+				numCorrect:st.numCorrect + 1
+			}
+		},()=>{this.nextQuestion()})
+		
 	}
 	incorrectQuestion= () => {
+		this.setState((st)=>{
+			return {
+				numIncorrect:st.numIncorrect + 1
+			}
+		},()=>{this.nextQuestion()})
 	}
 	render() {
 		const {question,answer} = this.props.deck.questions[this.state.questionNum]
 		return (
 			<View style={styles.container}>
+				<View>
+					<Text>{this.state.numCorrect} correct</Text>
+					<Text>{this.state.numIncorrect} incorrect</Text>
+				</View>
 				<ShowCard question={question} answer={answer} />
 				<View style={styles.buttons}>
 					<TouchableOpacity style={styles.button} onPress={this.correctQuestion}>
